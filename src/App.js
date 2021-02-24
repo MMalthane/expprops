801b4ba1e779190309c0e8e8821408f44de4217f
Meenakshi Malthane <mpachpol@gmail.com>
2021-02-24 23:04:16 +0000

    next commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,8 @@ import './App.css';
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <h1>Props Example </h1>
-=======
     <div className="App">
      <h1>Hello world </h1>
->>>>>>> 24e048e793d3335bc4be36e266748f2fdfe070e9
     </div>
   )
 }
